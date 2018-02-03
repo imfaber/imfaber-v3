246957256e14bb51ea6e3607ecde680d86a0f0e9
Fabrizio Meinero <imfaberdev@gmail.com>
2018-02-03 08:07:25 +0000

    Update og metatags

--- plugins/page-mixins.js
+++ plugins/page-mixins.js
@@ -16,20 +16,20 @@ const PageMixins = {
         if (this.meta && this.meta.title) {
           head.title = `${this.meta.title} | Fabrizio Meinero - Imfaber`
           head.meta.push({hid: 'twitter:title', name: 'twitter:title', content: this.meta.title})
-          head.meta.push({hid: 'og:title', name: 'twitter:title', content: this.meta.title})
+          head.meta.push({hid: 'og:title', name: 'og:title', content: this.meta.title})
         }
         if (this.meta && this.meta.description) {
           head.meta.push({hid: 'description', name: 'description', content: this.meta.description})
           head.meta.push({hid: 'twitter:description', name: 'twitter:description', content: this.meta.description})
-          head.meta.push({hid: 'og:description', name: 'twitter:description', content: this.meta.description})
+          head.meta.push({hid: 'og:description', name: 'og:description', content: this.meta.description})
         }
         if (this.meta && this.meta.image) {
           head.meta.push({hid: 'twitter:image', name: 'twitter:image', content: this.meta.image})
-          head.meta.push({hid: 'og:image', name: 'twitter:image', content: this.meta.image})
+          head.meta.push({hid: 'og:image', name: 'og:image', content: this.meta.image})
         }
         if (this.meta && this.meta.url) {
           head.meta.push({hid: 'twitter:url', name: 'twitter:url', content: this.meta.url})
-          head.meta.push({hid: 'og:url', name: 'twitter:url', content: this.meta.url})
+          head.meta.push({hid: 'og:url', name: 'og:url', content: this.meta.url})
           head.link.push({hid: 'canonical', name: 'canonical', href: this.meta.url})
         }
         if (this.meta && this.meta.themeColor) {
