a8406ba809eb89b024f81e64e56196c9e6e5ab01
Fabrizio Meinero <imfaberdev@gmail.com>
2018-03-29 12:44:18 +0200

    Update vue-stickykit

--- plugins/vue-stickykit.js
+++ plugins/vue-stickykit.js
@@ -1,22 +1,4 @@
 import Vue from 'vue'
-import $ from 'jquery'
-
-const VueStickyKit = {
-  /**
-   * install function
-   * @param  {Vue} Vue
-   */
-  install (Vue) {
-    Vue.directive('stick-in-parent', {
-      bind (el, binding, vnode, oldVnode) {
-        window.jQuery = $
-        require('sticky-kit/dist/sticky-kit.js')
-        $(document).ready(function() {
-          $(el).stick_in_parent(binding.value)
-        })
-      }
-    })
-  }
-}
+import VueStickyKit from 'vue-stickykit'
 
 Vue.use(VueStickyKit)
